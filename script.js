ae9f37de1c8215e3c46880eb9a9d255044a07524
washington gama <wgamaneto16@gmail.com>
2022-07-04 15:57:10 -0300

    paletas de cores2

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let paleta = document.getElementsByClassName('color');
 console.log(paleta)
-paleta[0].style.backgroundColor = 'black'
 paleta[1].style.backgroundColor = 'red'
 paleta[2].style.backgroundColor = 'blue'
 paleta[3].style.backgroundColor = 'green'
