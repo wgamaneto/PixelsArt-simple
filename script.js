4569195ffe341fbd0bc65390786b20f12ca803d6
washington gama <wgamaneto16@gmail.com>
2022-07-04 15:54:03 -0300

    paletas de cores

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-//criar divs pelo for loop
-const main = document.querySelector('main');
+let paleta = document.getElementsByClassName('color');
+console.log(paleta)
+paleta[0].style.backgroundColor = 'black'
+paleta[1].style.backgroundColor = 'red'
+paleta[2].style.backgroundColor = 'blue'
+paleta[3].style.backgroundColor = 'green'
 
-let criarDivs = '';
 
-for (let caixas = 0; caixa <= 10; caixas += 1) {
-    criarDivs += `<div>${caixas}</div>`;
-}
 
-main.innerHTML = criarDivs;
+// //criar divs pelo for loop
+// const main = document.querySelector('main');
+
+// let criarDivs = '';
+
+// for (let caixas = 0; caixas <= 10; caixas += 1) {
+//     criarDivs += `<div>${caixas}</div>`;
+// }
+
+// main.innerHTML = criarDivs;
