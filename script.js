03fdbeb8136988f1d6d836c8f51cfe8e3a56d132
washington gama <wgamaneto16@gmail.com>
2022-07-04 17:49:51 -0300

    paletas de cores4

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ paleta[2].style.backgroundColor = 'blue'
 paleta[3].style.backgroundColor = 'green'
 
 
+let quadrados = document.getElementById('pixel-board')
+for (let index = 1; index <= 5; index += 1){
+        let criarDivs = document.getElementById('div');
+        criarDivs.className = 'pixel'
+        quadrados.appendChild(criarDivs)
+    }   
+    
